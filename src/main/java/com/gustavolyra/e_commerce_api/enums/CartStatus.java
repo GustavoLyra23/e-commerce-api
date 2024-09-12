4ba1839af73cd1cfdf73bf78c0d392d408e307c8
GustavoLyra23 <gustavolyra23@gmail.com>
2024-09-12 19:44:03 -0300

    merge with gustavo branch

--- src/main/java/com/gustavolyra/e_commerce_api/enums/CartStatus.java
+++
@@ -1,9 +0,0 @@
-package com.gustavolyra.e_commerce_api.enums;
-
-public enum CartStatus {
-
-    OPEN,
-    PAID
-
-
-}
--- src/main/java/com/gustavolyra/e_commerce_api/repositories/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gustavolyra.e_commerce_api.repositories;
-
-import com.gustavolyra.e_commerce_api.entities.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
